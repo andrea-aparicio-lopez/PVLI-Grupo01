04a5262450ad237a072bc684db6de6d89589b916
guillesc05 <149385650+guillesc05@users.noreply.github.com>
2024-11-09 13:01:46 +0100

    actualizado game

--- docs/src/Game.js
+++ docs/src/Game.js
@@ -1,27 +1,60 @@
 import Board from './board/Board.js'
+import Entity from './objects/entity.js'
+import Enemy from './objects/enemies/enemy.js'
+import Ally from './objects/allies/ally.js'
+
 export default class Game extends Phaser.GameObjects.GameObject {
     //yo aqui le pasaria el tablero y que cree los turnos en base al tablero
     constructor(board) {
-        this.turnList;
-        this.turn = 0;
+        this.entityList = [];
+
+        entityList[0] = new Ally(0,0,0,0,0,10,10);
+        entityList[1] = new Enemy(0, 0, 0, 0, 0, 10, 10);
     }
-    
 
-    startTurn(entityTurn) {
-        console.log(this.turn); 
-        
+    //reordena entityList usando el coste de la habilidad usada
+    calculateTurnOrder(cost) {
 
+    }
+
+    startTurn() {
+        /*
+        entity.Play();
+        */
+        console.log("Started Turn");
         this.endTurn();
     }
 
     endTurn() {
         if (!this.gameHasEnded()) {
-            this.turn++;
-            this.startTurn();
+            console.log("Ended Turn");
+            this.calculateTurnOrder();
+            this.startTurn(this.entityList[0]);
         }
     }
 
     gameHasEnded() {
+        var alliesAlive = false;
+        //checkeo de los aliados vivos
+        for (i = 0; i < this.entityList.length; i++) {
+            if (alliesAlive == false && this.entityList[i].AI == undefined) {
+                console.log("Aliado");
+                alliesAlive = this.entityList.health > 0;
+            }
+        }
+        
+        if (alliesAlive == false) return true;
+
+        //checkeo de enemigos vivos
+        var enemiesAlive = false;
+        for (i = 0; i < this.entityList.length; i++) {
+            if (enemiesAlive == false && this.entityList[i].AI != undefined) {
+                console.log("Enemigo");
+                enemiesAlive = this.entityList.health > 0;
+            }
+        }
+        if (enemiesAlive == false) return true;
+
         return false;
     }
     
