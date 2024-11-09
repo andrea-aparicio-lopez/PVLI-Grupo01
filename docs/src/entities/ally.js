8b75c63b67d315a4840a0a4b6e57ead3bb990900
Andrea Aparicio <apariciolopez.andrea@gmail.com>
2024-11-09 18:38:53 +0100

    Update ally.js

--- docs/src/entities/ally.js
+++ docs/src/entities/ally.js
@@ -3,15 +3,14 @@ import Entity from "../entity.js";
 export default class Ally extends Entity {
     constructor(scene, x, y, texture, frame, health, maxHealth) {
         super(scene, x, y, texture, frame, health, maxHealth);
+
+        this.isFree = false;
     }
+
+    setFree() {this.isFree = true;}
 
     preupdate(t, dt) {
         super.preUpdate(t, dt);
-    }
-
-    /** @summary Llama al player para jugar su turno */
-    playTurn() {
-        super.playTurn();
     }
 
     die() {
