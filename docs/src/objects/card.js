68d94be3266fb07b99710a87bf6a66d0e36739f3
hdebrito92 <hdebrito@ucm.es>
2024-11-11 11:20:54 +0100

    card play/visualize, deck regenerate graveyard

--- docs/src/objects/card.js
+++ docs/src/objects/card.js
@@ -1,10 +1,9 @@
 
-
 export default class Card 
 {
     	/**
 	 * Constructor de Carta
-	 * @param {Player} player
+	 * @param {Scene} scene
 	 * @param {number} id
 	 * @param {string} name
 	 * @param {string} text
@@ -15,8 +14,10 @@ export default class Card
 	 * @param {boolean} stun
      * 
 	 */
-    constructor({ id, name, text, move, damage, areaOfEffect, range, stun }, scene) 
+    constructor(scene, { id, name, text, move, damage, areaOfEffect, range, stun }) 
     {
+        this.scene = scene;
+
         this.id = id;
         this.name = name;
         this.text = text;
@@ -24,56 +25,84 @@ export default class Card
 		this.damage = damage;
 		this.areaOfEffect = areaOfEffect;
 		this.range = range;
-		this.stun = stun;
-
-        this.scene = scene;
-		// this.entity = entity;
-
-        //if (this.damage != 0 && this.move != 0) this.range = this.move;
+		this.stun = stun;        
     }
 
-	play(entity)
-	{
+    // Visualize set to false by default
+	playedBy(entity, visualize = false) {
+        //console.log("Card effect played");
+                
         let direction = entity.getDirection();
         let currPosition = entity.getWorldPos();
+        
+        let rectsPositions = [];
 
-        // Movement
-        if(this.move != 0){
-            // console.log("Current position:", currPosition);
-            let newPos = {x: currPosition.x + this.move * direction.x, y: currPosition.y + this.move * direction.y};
-            entity.setWorldPos(newPos);
-            // console.log("New position after move:", newPosX);
-        }
-
-        // Damage
+        // DAMAGE
         if (this.damage != 0){
             // Spawn rects at adjacent positions on all directions
             if (this.areaOfEffect){
-                let rectsPositions = [];
                 for (let i = - this.range; i <= this.range; i++){
                     for (let j = - this.range; j <= this.range; j++){
-                        if (i != 0 || j != 0)
-                        {
+                        if (i != 0 || j != 0) {
                             rectsPositions.push({x: currPosition.x + i, y: currPosition.y + j})
                         }
                     }
                 }
-                //console.log("Spawned damage RectsPositions", rectsPositions);
+                // console.log("Spawned damage RectsPositions", rectsPositions);
             }
             // Spawned rects at adjacent position on facing direction and within range
             else {
-                let rectsPositions = [];
                 for (let k = 1; k <= this.range; k++){
                     rectsPositions.push({x: currPosition.x + k * direction.x, y: currPosition.y + k * direction.y});
-                    }
-                console.log("Spawned damage rect:", rectsPositions);
+                }                
+                //console.log("Spawned damage rect:", rectsPositions);
             }
 
-            /** @TODO Scene create rect group with ovelay and without gravity */ 
-            // create rect (position, stun)
+            this.changeRects(rectsPositions, visualize, 0xff0000);
+
         }
 
+        // Movement
+        if(this.move != 0){
+            //console.log("Current position:", currPosition);
+            let newPos = [];
+            newPos.push({x: currPosition.x + this.move * direction.x, y: currPosition.y + this.move * direction.y});
+
+            // If not in viasualization mode, update position
+            if (!visualize) entity.setWorldPos(newPos);
+            //console.log("New position after move:", newPos);
+            this.changeRects(newPos, visualize, 0x04ffA0);
+        }
 	}
+
+    visualizePlay(entity) {
+        //console.log("Visualizing play");
+
+        this.playedBy(entity, true);
+	}
+
+    changeRects(rects, visualize, color) {
+        
+        rects.forEach((rect) => {
+            let j = rect.x;
+            let i = rect.y;
+
+            if (i >= 0 && j >=0) {
+                let rectObj = this.scene.damageRectsGroup.children.entries[i * 10 + j];
+                rectObj.deactivate();
+                
+                if(visualize){
+                    rectObj.makeVisible();
+                    rectObj.fillColor = color;
+                }
+                else {
+                    rectObj.activate();
+                    rectObj.fillColor = color;
+                }
+            }
+        });
+        
+    }
 
     /** @summary Recieves the entity that was hit. Applies damage and stun if exists. Use as callback function for collision between entity and damage rects*/
     hit(entity){
--- docs/src/objects/deck.js
+++ docs/src/objects/deck.js
@@ -2,36 +2,32 @@ import Card from "./card.js"
 
 export default class Deck 
 {
-    constructor(cardsData, scene) 
+    constructor(scene, cardsData) 
     {
-        this.deckTemplate = cardsData.map(cardData => new Card(cardData, scene));
-        this.currentDeck = [];
+        //this.deckTemplate = cardsData.map(cardData => new Card(cardData, scene));
+        this.currentDeck = cardsData.map(cardData => new Card(scene, cardData));
+
         this.deckBurns = 0;
 
-        this.regenerate();
-    }
-
-    // Removes 1st card from Deck and returns it
-    drawCard()
-    {
-        if (this.currentDeck.length == 0){
-            this.regenerate();
-            this.deckBurns++;
-        }
-
-        return this.currentDeck.shift();
-    }
-
-    // Regenerates deck from template and shuffles it
-    regenerate()
-    {
-        this.deckTemplate.forEach((element) => this.currentDeck.push(element));
-        
         this.shuffle();
     }
 
-    shuffle() 
-    {
+    // Removes 1st card from Deck and returns it
+    draw() { return this.currentDeck.shift(); }
+
+    empty() { return this.currentDeck.length == 0; }
+
+    // Regenerates deck and shuffles it
+    regenerate(graveyard) {
+        let size = graveyard.length;
+        for(let i = size - 1; i >= 0 ; i--){
+            this.currentDeck.push(graveyard.splice(0, 1)[0]); 
+        }
+        this.deckBurns++;
+        this.shuffle();
+    }
+
+    shuffle() {
         let currentIndex = this.currentDeck.length - 1;
       
         while (currentIndex != 0) {
