395c840dcdcdeb9c8f3b98b87778f4558a01eaa5
guillesc05 <149385650+guillesc05@users.noreply.github.com>
2024-12-12 23:23:28 +0100

    enemoigops updated

--- docs/src/entities/enemy.js
+++ docs/src/entities/enemy.js
@@ -4,7 +4,7 @@ import Entity from "./entity.js";
 export default class Enemy extends Entity {
     constructor(scene, id, x, y, texture, frame, maxHealth) {
         super(scene, id, x, y, texture, frame, maxHealth);
-        this.health = 1;
+        this.health = 3;
     }
 
     preUpdate(t, dt) {
